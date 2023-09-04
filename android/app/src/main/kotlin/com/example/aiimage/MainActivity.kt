8f318d428cc6ae42506c9fe4020f827ebae3d683
gitInaaaJadiMaba <kfattah30@gmail.com>
2023-09-04 08:22:04 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/aiimage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aiimage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/item.dart
@@ -0,0 +1,42 @@
+List<Item> items = [
+  Item(
+    "https://img.freepik.com/free-photo/rise-humanoids-with-advanced-headgeargenerative-ai_8829-2877.jpg",
+    "The rise of humanoids",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/cat-warriors-with-burning-eyesgenerative-ai_8829-2909.jpg",
+    "Cat warriors",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/majestic-grand-piano-with-red-lightsgenerative-ai_188544-7789.jpg",
+    "Majestic grand piano",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/staircase-that-led-light-was-like-bridgeworlds-generative-ai_8829-2919.jpg",
+    "Road to light",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/refreshing-citrus-cocktail-garnishedwith-juicy-fruit-slices-generative-ai_188544-12394.jpg",
+    "Refreshing cocktail",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/glass-red-wine-cups-close-up-generativeai_188544-12239.jpg",
+    "Glass of wine",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/beauty-product-exploding-with-vibrantcolors-powder-generative-ai_188544-9687.jpg",
+    "Еxplosion of colors",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/abstract-glowing-flame-drops-electricillumination-generative-ai_188544-8092.jpg",
+    "Flame drops",
+  ),
+];
+
+class Item {
+  final String imageUrl;
+
+  final String name;
+
+  Item(this.imageUrl, this.name);
+}
